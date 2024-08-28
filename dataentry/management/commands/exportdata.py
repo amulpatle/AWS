c4534e91b4289796a68daa3b09c1956325c5ff65
amul patle <amulpatle3@gmail.com>
2024-08-29 00:14:56 +0530

    export data from single table

--- dataentry/management/commands/exportdata.py
+++ dataentry/management/commands/exportdata.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     
     def handle(self,*args,**kwargs):
         # fatch the data from the database
-        student = Student.objects.all()
+        students = Student.objects.all()
         
         # generate the timestampof current data and time
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%-M-%S")
@@ -17,7 +17,17 @@ class Command(BaseCommand):
         
         # define the csv file nama/path
         file_path = f'exported_student_data_{timestamp}.csv'
-        print(file_path)
+        
         
         # open the csv file and write the data
+        with open(file_path,'w',newline='') as file:
+            writer = csv.writer(file)
+            
+            # write the csv header
+            writer.writerow(['Roll No','Name','Age'])
+            
+            # write data rows
+            for student in students:
+                writer.writerow([student.roll_no,student.name,student.age])
         
+        self.stdout.write(self.style.SUCCESS('Data exporte successfully'))
