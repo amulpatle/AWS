f3e3af537c68dad7c83dc085f0144bc58358770f
amul patle <amulpatle3@gmail.com>
2024-08-29 20:36:17 +0530

    trigger the command from view

--- dataentry/utils.py
+++ dataentry/utils.py
@@ -2,7 +2,7 @@ from django.apps import apps
 
 def get_all_custom_models():
     # Try to get all the apps
-    default_models = ['ContentType','Session','LogEntry','Group','Permission','User']
+    default_models = ['ContentType','Session','LogEntry','Group','Permission','User','Upload']
     custom_models = []
     for model in apps.get_models():
         if model.__name__ not in default_models:
