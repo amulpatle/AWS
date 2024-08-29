2aec568864d8871d0d47107224c2e8f1b8b15971
amul patle <amulpatle3@gmail.com>
2024-08-29 23:01:29 +0530

    add 1M employee records

--- dataentry/models.py
+++ dataentry/models.py
@@ -19,3 +19,17 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.customer_name
+    
+
+class Employee(models.Model):
+    employee_id = models.IntegerField()
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    department = models.CharField(max_length=50)
+    salary = models.DecimalField(max_digits=10,decimal_places=2)
+    joining_date = models.DateField(max_length=10)
+    email_iD = models.EmailField()
+    address = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
