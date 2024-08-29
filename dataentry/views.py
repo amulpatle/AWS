6b42d603308493971dd0d733596309a439408d6b
amul patle <amulpatle3@gmail.com>
2024-08-29 20:52:31 +0530

    Display messages

--- dataentry/views.py
+++ dataentry/views.py
@@ -3,6 +3,7 @@ from .utils import get_all_custom_models
 from uploads.models import Upload
 from django.conf import settings
 from django.core.management import call_command
+from django.contrib import messages
 # Create your views here.
 
 def import_data(request):
@@ -22,8 +23,10 @@ def import_data(request):
         # trigger the importdata command
         try:
             call_command('importdata',file_path,model_name)
+            messages.success(request,'Data imported successfully')
         except Exception as e:
-            raise e
+            messages.error(request,str(e))
+            
         return redirect('import_data')
     else:
         custom_models = get_all_custom_models()
