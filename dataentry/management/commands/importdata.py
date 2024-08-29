85c486dbe1cbd5ba61a37eb8ee8e1877ac490fa8
amul patle <amulpatle3@gmail.com>
2024-08-29 21:09:23 +0530

    Handling import Error

--- dataentry/management/commands/importdata.py
+++ dataentry/management/commands/importdata.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand,CommandError
 from dataentry.models import Student
 import csv
 from django.apps import apps
+from django.db import DataError
 
 class Command(BaseCommand):
     help = "Import data from CSV file"
@@ -31,9 +32,19 @@ class Command(BaseCommand):
         if not model:
             raise CommandError(f'Model "{model_name}" not found in any app!')
         
+        # compare csv header with model's field names
+        # get all the field name of the model that we found
+        
+        model_fields = [field.name for field in model._meta.fields  if field.name  != 'id']
+        
         with open(file_path,'r')as file:
             reader = csv.DictReader(file)
-            print(reader)
+            csv_header = reader.fieldnames
+            
+            # compare csv header with model's field names
+            if csv_header != model_fields:
+                raise DataError(f"CSV file doesn't match with the {model_name } table fields.")
+            
             for row in reader:
                 model.objects.create(**row)
         self.stdout.write(self.style.SUCCESS("Data imported from csv successfully!"))
