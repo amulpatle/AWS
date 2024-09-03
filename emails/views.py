44cc9c6ec929d80f64cabe5ef1f43a93e9f468a7
amul patle <amulpatle3@gmail.com>
2024-09-03 20:55:36 +0530

    send bulk email

--- emails/views.py
+++ emails/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import EmailForm
+from django.contrib import messages
+from dataentry.utils import send_email_notificaton
+from django.conf import settings
 # Create your views here.
 
 def send_email(request):
-    return render(request,'emails/send-email.html')
+    if request.method == 'POST':
+        email_form = EmailForm(request.POST,request.FILES)
+        if email_form.is_valid():
+            email_form.save()
+
+            # send email
+            mail_subject = request.POST.get('subject')
+            message = request.POST.get('body')
+            to_email = settings.DEFAULT_TO_EMAIL
+            send_email_notificaton(mail_subject,message,to_email)
+            messages.success(request,'Email send successfully!')
+            return redirect('send_email')
+            
+    else:
+        email_form = EmailForm()
+        context = {
+            'email_form':email_form
+        }
+        return render(request,'emails/send-email.html',context)
