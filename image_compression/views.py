35126c35480a770a874accc0a2e7bb3df2402650
amul patle <amulpatle3@gmail.com>
2024-09-07 01:11:51 +0530

    image compression form

--- image_compression/views.py
+++ image_compression/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .forms import CompressImageForm
 # Create your views here.
 
 def compress(request):
-    return render(request,'image_compression/compress.html')
+    form = CompressImageForm()
+    context = {
+        'form':form
+    }
+    return render(request,'image_compression/compress.html',context)
