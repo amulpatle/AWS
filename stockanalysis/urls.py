86a0b470beaff8106b239ad518a8511ff80d4791
amul patle <amulpatle3@gmail.com>
2024-09-10 23:11:56 +0530

    setup Beautiful soup code'

--- stockanalysis/urls.py
+++ stockanalysis/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from. import views
 urlpatterns = [
     path('stocks/',views.stocks,name='stocks'),
+    path('stock-autocomplete/', views.StockAutocomplete.as_view(), name="stock_autocomplete"),
 ]
--- stockanalysis/views.py
+++ stockanalysis/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from dal import autocomplete
+from .models import Stock
+from .forms import StockForm
 # Create your views here.
 
 def stocks(request):
-    return render(request,'stockanalysis/stocks.html')
+    form = StockForm()
+    context = {
+        'form':form,
+    }
+    return render(request,'stockanalysis/stocks.html',context)
+
+
+class StockAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        qs = Stock.objects.all()
+        
+        if self.q:
+            print('entered keyword=>', self.q)
+            qs = qs.filter(Name__istartswith=self.q)
+            print('result==>', qs)
+
+        return qs
